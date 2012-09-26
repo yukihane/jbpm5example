e48035914bc8c246d461e83c2c831fd05fac94fa
yukihane <yukihane.feather@gmail.com>
2012-09-26 17:34:21 +0900

    WebServlet annotationを使用する

--- rewards-basic/war/src/main/java/com/sample/TaskServlet.java
+++ rewards-basic/war/src/main/java/com/sample/TaskServlet.java
@@ -7,12 +7,14 @@ import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jbpm.task.query.TaskSummary;
 
+@WebServlet(urlPatterns="/task")
 public class TaskServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
