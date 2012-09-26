153b202c76961509efdddbab38d79ab41bcec0af
yukihane <yukihane.feather@gmail.com>
2012-09-26 17:39:59 +0900

    annotationを他にあわせるよう修正

--- rewards-basic/war/src/main/java/com/sample/LoginServlet.java
+++ rewards-basic/war/src/main/java/com/sample/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "LoginServlet", urlPatterns = { "/login" })
+@WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
