35f720a9c0388ddf52d2c8f90cedc65ff665234d
yukihane <yukihane.feather@gmail.com>
2013-02-25 15:31:53 +0900

    親クラス(AbstractHTWorkItemHandler)のメソッドをコピー

--- rewards-basic/ejb/src/main/java/com/sample/MyKnowledgeBase.java
+++ rewards-basic/ejb/src/main/java/com/sample/MyKnowledgeBase.java
@@ -1,5 +1,7 @@
 package com.sample;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -9,7 +11,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 import org.jbpm.process.audit.JPAWorkingMemoryDbLogger;
+import org.jbpm.process.workitem.wsht.HumanTaskHandlerHelper;
 import org.jbpm.process.workitem.wsht.LocalHTWorkItemHandler;
+import org.jbpm.process.workitem.wsht.PeopleAssignmentHelper;
+import org.jbpm.task.I18NText;
+import org.jbpm.task.OrganizationalEntity;
+import org.jbpm.task.PeopleAssignments;
+import org.jbpm.task.SubTasksStrategy;
+import org.jbpm.task.SubTasksStrategyFactory;
+import org.jbpm.task.Task;
+import org.jbpm.task.TaskData;
+import org.jbpm.task.User;
 import org.jbpm.task.service.TaskService;
 import org.jbpm.task.service.local.LocalTaskService;
 import org.kie.KieBase;
@@ -114,6 +126,96 @@ public class MyKnowledgeBase {
             Map<String, Object> results = workItem.getResults();
             System.out.println("results: " + results);
             super.executeWorkItem(workItem, manager);
+        }
+
+        @Override
+        protected Task createTaskBasedOnWorkItemParams(WorkItem workItem) {
+            Task task = new Task();
+            String locale = (String) workItem.getParameter("Locale");
+            if (locale == null) {
+                locale = "en-UK";
+            }
+            String taskName = (String) workItem.getParameter("TaskName");
+            if (taskName != null) {
+                List<I18NText> names = new ArrayList<I18NText>();
+                names.add(new I18NText(locale, taskName));
+                task.setNames(names);
+            }
+            String comment = (String) workItem.getParameter("Comment");
+            if (comment == null) {
+                comment = "";
+            }
+            List<I18NText> descriptions = new ArrayList<I18NText>();
+            descriptions.add(new I18NText(locale, comment));
+            task.setDescriptions(descriptions);
+            List<I18NText> subjects = new ArrayList<I18NText>();
+            subjects.add(new I18NText(locale, comment));
+            task.setSubjects(subjects);
+            String priorityString = (String) workItem.getParameter("Priority");
+            int priority = 0;
+            if (priorityString != null) {
+                try {
+                    priority = new Integer(priorityString);
+                } catch (NumberFormatException e) {
+                    // do nothing
+                }
+            }
+            task.setPriority(priority);
+            TaskData taskData = new TaskData();
+            taskData.setWorkItemId(workItem.getId());
+            taskData.setProcessInstanceId(workItem.getProcessInstanceId());
+            if (session != null
+                    && session.getProcessInstance(workItem
+                            .getProcessInstanceId()) != null) {
+                taskData.setProcessId(session
+                        .getProcessInstance(workItem.getProcessInstanceId())
+                        .getProcess().getId());
+            }
+            if (session != null
+                    && (session instanceof StatefulKnowledgeSession)) {
+                taskData.setProcessSessionId(((StatefulKnowledgeSession) session)
+                        .getId());
+            }
+            taskData.setSkipable(!"false".equals(workItem
+                    .getParameter("Skippable")));
+            // Sub Task Data
+            Long parentId = (Long) workItem.getParameter("ParentId");
+            if (parentId != null) {
+                taskData.setParentId(parentId);
+            }
+            String subTaskStrategiesCommaSeparated = (String) workItem
+                    .getParameter("SubTaskStrategies");
+            if (subTaskStrategiesCommaSeparated != null
+                    && !subTaskStrategiesCommaSeparated.equals("")) {
+                String[] subTaskStrategies = subTaskStrategiesCommaSeparated
+                        .split(",");
+                List<SubTasksStrategy> strategies = new ArrayList<SubTasksStrategy>();
+                for (String subTaskStrategyString : subTaskStrategies) {
+                    SubTasksStrategy subTaskStrategy = SubTasksStrategyFactory
+                            .newStrategy(subTaskStrategyString);
+                    strategies.add(subTaskStrategy);
+                }
+                task.setSubTaskStrategies(strategies);
+            }
+
+            String createdBy = (String) workItem.getParameter("CreatedBy");
+            if (createdBy != null && createdBy.trim().length() > 0) {
+                taskData.setCreatedBy(new User(createdBy));
+            }
+
+            task.setTaskData(taskData);
+
+            PeopleAssignmentHelper peopleAssignmentHelper = new PeopleAssignmentHelper();
+            peopleAssignmentHelper.handlePeopleAssignments(workItem, task,
+                    taskData);
+
+            PeopleAssignments peopleAssignments = task.getPeopleAssignments();
+            List<OrganizationalEntity> businessAdministrators = peopleAssignments
+                    .getBusinessAdministrators();
+
+            task.setDeadlines(HumanTaskHandlerHelper.setDeadlines(workItem,
+                    businessAdministrators, session.getEnvironment()));
+            return task;
         }
     }
 }
