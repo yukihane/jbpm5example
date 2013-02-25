dc20d5e01b8b2738e9aa9c743bf031176ea16dcf
yukihane <yukihane.feather@gmail.com>
2013-02-25 09:06:53 +0900

    import再編成

--- rewards-basic/war/src/main/java/com/sample/ProcessServlet.java
+++ rewards-basic/war/src/main/java/com/sample/ProcessServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sample.entity.MyContent;
-
 @WebServlet(urlPatterns="/process")
 public class ProcessServlet extends HttpServlet {
 
